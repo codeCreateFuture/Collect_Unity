4f52087aa91eb490e993cf5c545db03ead4d3deb
codeCreateFuture <66620003+codeCreateFuture@users.noreply.github.com>
2021-08-31 18:35:46 +0800

    导入的图片自动变成Sprite
    导入的图片自动变成Sprite

---
+++ Editor/Auto/AssetPostManager_Editor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class AssetPostManager_Editor : AssetPostprocessor
+{
+    void OnPostprocessTexture(Texture texture)
+    {
+        //if (assetPath.StartsWith("Assets/Resources/Image"))
+        //{
+            TextureImporter importer = AssetImporter.GetAtPath(assetPath) as TextureImporter;
+            if ((importer != null) && (importer.textureType != TextureImporterType.Sprite))
+            {
+                importer.textureType = TextureImporterType.Sprite;
+                importer.SaveAndReimport();
+            }
+       // }
+    }
+}
